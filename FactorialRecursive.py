bc263947e47b0006ce40255b617a4dd74018c173
LuckylisaBemeye <166706078+LuckylisaBemeye@users.noreply.github.com>
2025-04-26 18:16:42 +0300

    Update FactorialRecursive.py

--- FactorialRecursive.py
+++ FactorialRecursive.py
@@ -1,11 +1,11 @@
 def factorial(num):
     if num==1:
-        return num*1
+        return num*1  #base case
     else:    
-        return num* factorial(num-1)
+        return num* factorial(num-1) #recursive case
 
     print(factorial)
 
-x=int(input("Enter the number whose factorial you want: "))
+x=int(input("Enter the number whose factorial you want: "))  #specifying the input is an integer rather than a string as the imput function defaults it to
 result=factorial(x)        
 print(x, "! = ",result)
